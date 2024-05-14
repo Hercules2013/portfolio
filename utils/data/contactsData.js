58574b925028073cae7b482a6ba03cb6655eb1dc
Hercules <heracules2347@gmail.com>
2024-05-14 20:42:40 +0800

    feat: init project

---
+++ utils/data/contactsData.js
@@ -0,0 +1,11 @@
+export const contactsData = {
+    email: 'calimameh@gmail.com',
+    phone: '+8801608797655',
+    address: 'Napoli, Italy',
+    github: 'https://github.com/hercules2013',
+    // facebook: 'https://www.facebook.com/abusaid.riyaz/',
+    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
+    // twitter: 'https://twitter.com/said7388',
+    // stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+    devUsername: "hercules2013"
+}
---
+++ utils/data/personal-data.js
@@ -0,0 +1,19 @@
+export const personalData = {
+  name: "Alimameh Ceesay",
+  profile: '/profile.png',
+  designation: "Software Engineer",
+  description: "My name is Alimameh Ceesay. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: 'calimameh@gmail.com',
+  phone: '+393474551765',
+  address: 'Napoli, Italy',
+  github: 'https://github.com/hercules2013',
+  skype: 'skype:live:.cid.cccf2e71253ff8f?add',
+  // facebook: 'https://www.facebook.com/abusaid.riyaz/',
+  telegram: 'https://t.me/alimameh',
+  linkedIn: 'https://www.linkedin.com/in/alimameh-ceesay-5a9627301/',
+  // twitter: 'https://twitter.com/said7388',
+  stackOverflow: 'https://stackoverflow.com/users/25033149/alimameh-ceesay',
+  // leetcode: "https://leetcode.com/said3812/",
+  devUsername: "hercules2013",
+  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+}
