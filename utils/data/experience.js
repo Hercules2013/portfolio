d3deb50f92a8d832672f94d8781c50648065566e
Hercules <heracules2347@gmail.com>
2024-05-16 21:16:21 +0800

    feat: complete portfolio

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,20 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Engineer I',
-    company: "Teton Private Ltd.",
-    duration: "(Jan 2022 - Present)"
+    title: 'Senior Front-End Engineer',
+    company: "SimStudio",
+    duration: "(Aug 2021 - Feb 2024)"
   },
   {
     id: 2,
-    title: "FullStack Developer",
-    company: "Fiverr (freelance)",
-    duration: "(Jun 2021 - Jan 2022)"
+    title: "Web Developer",
+    company: "Ingenia Direct",
+    duration: "(Oct 2019 - Jun 2021)"
   },
-  {
-    id: 3,
-    title: "Self Employed",
-    company: "Code and build something in everyday.",
-    duration: "(Jan 2018 - Present)"
-  }
+  // {
+  //   id: 3,
+  //   title: "Self Employed",
+  //   company: "Code and build something in everyday.",
+  //   duration: "(Jan 2018 - Present)"
+  // }
 ]
